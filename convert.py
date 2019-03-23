69fa66bfb5276420322004d56ab57e017c932b02
Arcticactus <Arcticactus@users.noreply.github.com>
2019-03-23 17:26:15 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -5,10 +5,6 @@ import img2pdf
 
 # Run with python convert.py
 
-# a4input = (img2pdf.mm_to_pt(50),img2pdf.mm_to_pt(10))
-# layout_fun = img2pdf.get_layout_fun(a4input)
-
-
 def create_pdf(source_images, pdf_name):
     if not pdf_name.endswith(".pdf"):
         print("pdf_name must end in .pdf.")
@@ -22,4 +18,4 @@ if __name__ == '__main__':
     images = [image for image in listdir(cwd) if isfile(join(cwd, image))]
     images = images[:-1]  # To ignore convert.py.
     # Assumption: convert.py is the last file in lexical order.
-    create_pdf(images, "キノの旅 第06巻.pdf")
+    create_pdf(images, "output.pdf")
